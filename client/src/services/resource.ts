4d4355578db4a17c70453dbe96576e07b7e052e7
Julian Hinsch <jhinsch799@gmail.com>
2022-11-27 20:33:23 -0500

    finalize store

--- client/src/services/resource.ts
+++ client/src/services/resource.ts
@@ -1,16 +1,68 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const resourceApi = createApi({
+  reducerPath: 'resourceApi',
   baseQuery: fetchBaseQuery({ baseUrl: '/api/resources' }),
+  tagTypes: ['Resource'],
   endpoints: (builder) => ({
-    getAll: builder.query({
+    getAllResources: builder.query({
       query: () => '',
+      providesTags: (result) =>
+      result
+        ? [
+            ...result.map(({ id }: { id: number }) => ({ type: 'Resource' as const, id })),
+            { type: 'Resource', id: 'LIST' },
+          ]
+        : [{ type: 'Resource', id: 'LIST' }],
     }),
-    // createOne:
-    // updateOne:
-    // deleteOne:
+    createResource: builder.mutation({
+      query: (data) => ({
+        url: '',
+        method: 'POST',
+        body: data,
+      }),
+      invalidatesTags: ['Resource']
+    }),
+    updateResource: builder.mutation({
+      query: ({ id, data }) => ({
+        url: `/${id}`,
+        method: 'PATCH',
+        body: data,
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      }),
+      /**
+       * Give the appearance that the UI has updated
+       * even though the request is still in-flight.
+       */
+      async onQueryStarted({ id, ...patch }, { dispatch, queryFulfilled }) {
+        const patchResult = dispatch(
+          resourceApi.util.updateQueryData('getAllResources', id, (draft) => {
+            Object.assign(draft, patch)
+          })
+        )
+        try {
+          await queryFulfilled
+        } catch {
+          patchResult.undo()
+        }
+      },
+      invalidatesTags: (result, error, { id }) => [{ type: 'Resource', id }],
+    }),
+    deleteResource: builder.mutation({
+      query: (id) => ({
+        url: `/${id}`,
+        method: 'DELETE',
+      }),
+      invalidatesTags: (result, error, id) => [{ type: 'Resource', id }],
+    })
   }),
 })
 
-// Export hooks for usage in functional components
-export const { useGetAllQuery } = resourceApi;
+export const {
+  useGetAllResourcesQuery,
+  useCreateResourceMutation,
+  useUpdateResourceMutation,
+  useDeleteResourceMutation
+} = resourceApi;
