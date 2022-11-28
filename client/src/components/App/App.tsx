38fc28a1b740a57a631effffa833d56040af2309
Julian Hinsch <jhinsch799@gmail.com>
2022-11-27 19:57:19 -0500

    App component

--- client/src/components/App/App.tsx
+++ client/src/components/App/App.tsx
@@ -1,10 +1,30 @@
+import { Alert, CircularProgress } from '@mui/material';
+
+import { useGetAllResourcesQuery } from '../../services/resource';
+
 import ResourceList from '../ResourceList/ResourceList';
-import '../../index.scss';
+import InfoPanel from '../InfoPanel/InfoPanel';
+
+import styles from './App.module.scss';
+import PieChartContainer from '../PieChartContainer/PieChartContainer';
 
 function App() {
+  const { data, error, isLoading } = useGetAllResourcesQuery('');
+
   return (
-    <div className="app">
-      <ResourceList />
+    <div className={styles.app}>
+      <div className={styles.header}>
+        <h1>Grid Manager</h1>
+      </div>
+      {isLoading && <CircularProgress />}
+      {error && <Alert variant="filled" severity='error'>Oops! Something went wrong.</Alert>}
+      {data && (
+        <div className={styles.dashboard}>
+          <ResourceList resources={data}/>
+          <PieChartContainer resources={data}/>
+          <InfoPanel resources={data}/>
+        </div>
+      )}
     </div>
   );
 }
