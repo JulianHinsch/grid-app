a172c5796e5842188c7b9721bba6fa74742aa296
Julian Hinsch <jhinsch799@gmail.com>
2022-11-27 20:33:00 -0500

    add resource list component

--- client/src/components/ResourceList/ResourceList.tsx
+++ client/src/components/ResourceList/ResourceList.tsx
@@ -1,12 +1,73 @@
-import { useGetAllQuery } from '../../services/resource';
+import { useState } from 'react';
+import Resource from '../ResourceDetail/ResourceDetail';
+import NewResourceDialog from '../NewResourceDialog/NewResourceDialog';
+import { Resource as ResourceType } from '../../types/resource';
+import {
+    Accordion,
+    AccordionSummary,
+    AccordionDetails,
+    Box,
+    Button,
+    Typography
+} from '@mui/material';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
-function ResourceList() {
-    const { data, error, isLoading } = useGetAllQuery('');
+import styles from './ResourceList.module.scss';
+import { getCurrentOutput } from '../../util';
+
+interface ResourceListProps {
+    resources: ResourceType[],
+}
+
+function ResourceList(props: ResourceListProps) {
+    const [newResourceDialogOpen, setNewResourceDialogOpen] = useState(false);
+
+    const handleNewResourceClick = () => {
+        setNewResourceDialogOpen(true);
+    }
+
+    const handleNewResourceDialogClose = () => {
+        setNewResourceDialogOpen(false);
+    }
 
     return (
-        <div className="resource-list">
-            {JSON.stringify(data)}
-        </div>
+        <Box className={styles['resource-list']}>
+            <Box className={styles.header}>
+                <h2 className={styles.heading}>
+                    Resources
+                </h2>
+                <Button
+                    variant="contained"
+                    disableElevation
+                    onClick={handleNewResourceClick}
+                    sx={{textTransform: "none"}}>
+                        New Resource
+                </Button>
+            </Box>
+            <NewResourceDialog
+                open={newResourceDialogOpen}
+                handleClose={handleNewResourceDialogClose}/>
+            {[...props.resources].sort((r1, r2) => r1.id - r2.id).map((resource, idx) => (
+                <Accordion key={idx}>
+                    <AccordionSummary
+                        expandIcon={<ExpandMoreIcon />}
+                    >
+                        <Typography sx={{ width: '33%', flexShrink: 0, fontWeight: 'bold '}}>
+                            {resource.nickname}
+                        </Typography>
+                        <Typography sx={{ width: '33%', flexShrink: 0, color: 'text.secondary', textTransform: 'capitalize' }}>
+                            {resource.type.toLowerCase()}
+                        </Typography>
+                        <Typography sx={{ width: '33%', flexShrink: 0, fontWeight: 'bold '}}>
+                            {getCurrentOutput(resource)}/{resource.max_output} kW
+                        </Typography>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        <Resource resource={resource}/>
+                    </AccordionDetails>
+                </Accordion>
+            ))}
+        </Box>
     )
 }
 
