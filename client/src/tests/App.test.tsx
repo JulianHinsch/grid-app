81cbdbfe064c5419679de224a0e33bd49c64f7c4
Julian Hinsch <jhinsch799@gmail.com>
2022-12-02 11:24:09 -0500

    add test stubs

--- client/src/tests/App.test.tsx
+++ client/src/tests/App.test.tsx
@@ -13,5 +13,17 @@ const store = setupStore();
 //     </Provider>
 //   );
 
-//   expect(sgetByText(/learn/i)).toBeInTheDocument();
+//   expect(getByText(/learn/i)).toBeInTheDocument();
 // });
+
+it('Renders loading spinner until data is loaded', () => {
+    // TODO
+});
+
+it('Renders error alert if data fetch fails', () => {
+    // TODO
+});
+
+it('Renders dashboard if data fetch succeeds', () => {
+    // TODO
+});
