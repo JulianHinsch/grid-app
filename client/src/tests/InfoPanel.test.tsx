90b3a9cd42528490bef4bbc1ff9af8a8500b4111
Julian Hinsch <jhinsch799@gmail.com>
2022-12-20 14:35:47 -0500

    Use .jsx not .tsx for test files

--- client/src/tests/InfoPanel.test.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import InfoPanel from '../components/InfoPanel/InfoPanel';
-
-it('should render correct totals based on resources prop', () => {
-    // TODO
-});
--- client/src/tests/ResourceDetail.test.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import ResourceDetail from '../components/ResourceDetail/ResourceDetail';
-
-it('Updates current output % when user moves slider', () => {
-    // TODO
-});
